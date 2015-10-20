1ec554356b2eee874fdfbf67a4bbe9d42011bf42
abauzac <amaury.bauzac@gmail.com>
2015-10-20 12:40:28 +0200

    moving background

--- Pumpkin/Pumpkin/app.js
+++ Pumpkin/Pumpkin/app.js
@@ -1,13 +1,22 @@
 var SimpleGame = (function () {
     function SimpleGame() {
-        this.game = new Phaser.Game(800, 600, Phaser.AUTO, "content", { preload: this.preload, create: this.create });
+        this.game = new Phaser.Game(800, 600, Phaser.AUTO, "content", { preload: this.preload, create: this.create, update: this.update });
     }
     SimpleGame.prototype.preload = function () {
-        this.game.load.image("logo", "Assets/phaser.png");
+        this.game.load.image("pumpkin", "Assets/pumpkin.png");
+        this.game.load.image("wall", "Assets/wall.png");
     };
     SimpleGame.prototype.create = function () {
-        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, "logo");
-        logo.anchor.setTo(0.5, 0.5);
+        // create background first
+        this.background = this.game.add.tileSprite(0, 0, 800, 600, 'wall');
+        this.pumpkin = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, "pumpkin");
+        this.pumpkin.anchor.setTo(0.5, 0.5);
+        this.pumpkin.width = 100;
+        this.pumpkin.height = 70;
+    };
+    SimpleGame.prototype.update = function () {
+        // background http://examples.phaser.io/_site/view_full.html?d=games&f=invaders.js&t=invaders
+        this.background.tilePosition.x -= 2;
     };
     return SimpleGame;
 })();
--- Pumpkin/Pumpkin/app.ts
+++ Pumpkin/Pumpkin/app.ts
@@ -1,18 +1,37 @@
 ﻿class SimpleGame {
 
     constructor() {
-        this.game = new Phaser.Game(800, 600, Phaser.AUTO, "content", { preload: this.preload, create: this.create });
+        this.game = new Phaser.Game(800, 600, Phaser.AUTO, "content", { preload: this.preload, create: this.create, update: this.update });
     }
+
+    public background: Phaser.TileSprite;
+    public pumpkin: Phaser.Sprite;
 
     game: Phaser.Game;
 
     preload() {
-        this.game.load.image("logo", "Assets/phaser.png");
+        this.game.load.image("pumpkin", "Assets/pumpkin.png");
+        this.game.load.image("wall", "Assets/wall.png"); 
     }
 
     create() {
-        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, "logo");
-        logo.anchor.setTo(0.5, 0.5);
+        // create background first
+        this.background = this.game.add.tileSprite(0, 0, 800, 600, 'wall');
+
+        this.pumpkin = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, "pumpkin");
+        this.pumpkin.anchor.setTo(0.5, 0.5);
+        this.pumpkin.width = 100;
+        this.pumpkin.height = 70;
+
+
+
+    }
+
+    update() {
+        // background http://examples.phaser.io/_site/view_full.html?d=games&f=invaders.js&t=invaders
+        this.background.tilePosition.x -= 2;
+
+
     }
 
 }
