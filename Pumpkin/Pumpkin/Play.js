534c9a3e6e27b602f68ae078e3769aa9afcdd052
Astyan42 <deveaux162@hotmail.com>
2015-10-30 13:12:55 +0100

    la corde suis la citrouille

--- Pumpkin/Pumpkin/Play.js
+++ Pumpkin/Pumpkin/Play.js
@@ -14,7 +14,7 @@ var Pumpkin;
             this.currentX = 0;
         }
         Play.prototype.preload = function () {
-            this.game.load.image('sprite', 'assets/corde.png');
+            this.game.load.image('rope', 'assets/corde.png');
             this.game.load.image("wall", "Assets/background_.jpg");
             this.game.load.image("pumpkin", "Assets/pumpkin.png");
             this.game.load.spritesheet("block", "Assets/block.png", 32, 32);
@@ -25,16 +25,35 @@ var Pumpkin;
             // create background first
             this.background = this.game.add.tileSprite(0, 0, 800, 600, 'wall');
             this.pumpkin = new Pumpkin.Pumpkin(this.game, this.game.world.centerX, this.game.world.centerY);
+            this.rope = this.game.add.tileSprite(this.pumpkin.x, this.pumpkin.y, 0, 10, "rope");
+            this.rope.alive = false;
+            this.rope.visible = false;
             this.pumpkin.width = 100;
             this.pumpkin.height = 70;
             this.physicGroupBlocks = this.game.add.physicsGroup(false);
             for (var i = 0; i < 30; i++) {
                 this.physicGroupBlocks.create(i * 32, 0, "block");
             }
+            this.spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+            this.game.input.keyboard.addKeyCapture(Phaser.Keyboard.SPACEBAR);
         };
         Play.prototype.update = function () {
             // background http://examples.phaser.io/_site/view_full.html?d=games&f=invaders.js&t=invaders
             this.background.tilePosition.x -= 2;
+            if (this.rope && this.rope.alive) {
+                this.rope.width += 25;
+                this.rope.x = this.pumpkin.x;
+                this.rope.y = this.pumpkin.y;
+            }
+            if (this.spaceKey.isDown && !this.spaceKey.downDuration()) {
+                this.shootRope();
+            }
+        };
+        Play.prototype.shootRope = function () {
+            this.rope.width = 0;
+            this.rope.alive = true;
+            this.rope.visible = true;
+            this.rope.rotation = this.game.physics.arcade.angleToPointer(this.rope);
         };
         return Play;
     })(Phaser.State);
--- Pumpkin/Pumpkin/Play.ts
+++ Pumpkin/Pumpkin/Play.ts
@@ -3,31 +3,32 @@
 module Pumpkin {
     export class Play extends Phaser.State {
         private tilesprite;
-        private rope: Phaser.Rope;
         public background: Phaser.TileSprite;
         public pumpkin: Phaser.Sprite;
         private currentX = 0;
         public physicGroupBlocks: Phaser.Group;
-
+        private rope: Phaser.TileSprite;
+        private spaceKey:Phaser.Key;
         game: Phaser.Game;
 
 
         preload() {
-            this.game.load.image('sprite', 'assets/corde.png');
+            this.game.load.image('rope', 'assets/corde.png');
             this.game.load.image("wall", "Assets/background_.jpg"); 
             this.game.load.image("pumpkin", "Assets/pumpkin.png");
             this.game.load.spritesheet("block", "Assets/block.png", 32, 32); 
         }
+
         create() {
-
-
             this.game.physics.startSystem(Phaser.Physics.ARCADE);
             this.game.physics.arcade.gravity.y = 400;
             // create background first
             this.background = this.game.add.tileSprite(0, 0, 800, 600, 'wall');
 
             this.pumpkin = new Pumpkin(this.game, this.game.world.centerX, this.game.world.centerY);
-            
+            this.rope = this.game.add.tileSprite(this.pumpkin.x, this.pumpkin.y, 0, 10, "rope");
+            this.rope.alive = false;
+            this.rope.visible = false;
             this.pumpkin.width = 100;
             this.pumpkin.height = 70;
             this.physicGroupBlocks = this.game.add.physicsGroup(<any>false);
@@ -38,11 +39,30 @@ module Pumpkin {
             }
 
 
+            this.spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+            this.game.input.keyboard.addKeyCapture(Phaser.Keyboard.SPACEBAR);
+
         }
         update() {
             // background http://examples.phaser.io/_site/view_full.html?d=games&f=invaders.js&t=invaders
             this.background.tilePosition.x -= 2;
+            if (this.rope && this.rope.alive) {
+                this.rope.width += 25;
+                this.rope.x = this.pumpkin.x;
+                this.rope.y = this.pumpkin.y;
+            }
+
+            if (this.spaceKey.isDown && !this.spaceKey.downDuration()) {
+                this.shootRope();
+            }
+
         }
-        
+
+        shootRope() {
+            this.rope.width = 0;
+            this.rope.alive = true;
+            this.rope.visible = true;
+            this.rope.rotation = this.game.physics.arcade.angleToPointer(this.rope);
+        }
     }
 }
